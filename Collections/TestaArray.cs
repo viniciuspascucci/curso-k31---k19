4731dfd989ab6620bc75d2eac94c08ec4dcced96
viniciuspascucci <vinicius.pascucci123123@gmail.com>
2015-07-04 16:35:44 -0300

    Pojeto do capito de Collections

---
+++ Collections/TestaArray.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Diagnostics;
+
+namespace Collections
+{
+	class TestaArray
+	{
+		static void Main(string[] args)
+		{
+			ArrayList nomes = new ArrayList();
+
+			long tempo = TestaArray.AdicionaNoFinal(nomes);
+			Console.WriteLine("ArrayList: " + tempo + " ms ");
+		}
+
+		public static long AdicionaNoFinal(IList lista)
+		{
+			Stopwatch sw = new Stopwatch();
+
+			sw.Start();
+			int size = 100000;
+
+			for (int i = 0; i < size; i++)
+			{
+				lista.Add(i);
+			}
+
+			sw.Stop();
+
+			return sw.ElapsedMilliseconds;
+		}
+	}
+}
---
+++ Collections/TestaArrayInicio.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Diagnostics;
+
+namespace Collections
+{
+	class TestaArrayInicio
+	{
+		static void Main()
+		{
+			ArrayList nomesInicio = new ArrayList();
+
+			long tempo = TestaArrayInicio.AdicionaNoComeco(nomesInicio);
+			Console.WriteLine("ArrayList: " + tempo + "ms");
+		}
+
+		public static long AdicionaNoComeco(IList lista)
+		{
+			Stopwatch sw = new Stopwatch();
+			sw.Start();
+			int size = 100000;
+
+			for (int i = 0; i < size; i++)
+			{
+				lista.Insert(0, 1);
+			}
+
+			sw.Stop();
+
+			return sw.ElapsedMilliseconds;
+		}
+	}
+}
