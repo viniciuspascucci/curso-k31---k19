55a4accd7d84fa04ec2e4ec0d37bc57985f371b9
viniciuspascucci <vinicius.pascucci123123@gmail.com>
2015-07-04 16:41:02 -0300

    Projeto do capito de Generic

---
+++ Gerenic/TestaAdcionaNoComeco.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Gerenic
+{
+	class TestaAdcionaNoComeco
+	{
+		static void Main(string[] args)
+		{
+			List<int> arrayList = new List<int>();
+
+			long time = TestaAdcionaNoComeco.AdicionaNoComecoArrayList(arrayList);
+			Console.WriteLine("ArrayList: " + time + "ms");
+
+			LinkedList<int> linkedList = new LinkedList<int>();
+
+			time = TestaAdcionaNoComeco.AdicionaNoComecoLinkedList(linkedList);
+			Console.WriteLine("LinkedList: " + time + "ms");
+		}
+
+		public static long AdicionaNoComecoArrayList(List<int> lista)
+		{
+			Stopwatch sw = new Stopwatch();
+			sw.Start();
+			int size = 100000;
+
+			for (int i = 0; i < size; i++)
+			{
+				lista.Insert(0, 1);
+			}
+
+			sw.Stop();
+
+			return sw.ElapsedMilliseconds;
+		}
+
+		public static long AdicionaNoComecoLinkedList(LinkedList<int> lista)
+		{
+			Stopwatch sw = new Stopwatch();
+			sw.Start();
+			int size = 100000;
+
+			for (int i = 0; i < size; i++)
+			{
+				lista.AddFirst(i);
+			}
+
+			sw.Stop();
+
+			return sw.ElapsedMilliseconds;
+		}
+	}
+}
---
+++ Gerenic/TestaAdcionaNoFinal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Gerenic
+{
+	class TestaAdcionaNoFinal
+	{
+		static void Main(string[] args)
+		{
+			List<int> arrayList = new List<int>();
+
+			long temp = TestaAdcionaNoFinal.AdicionaNoFinal(arrayList);
+			Console.WriteLine("ArrayList: " + temp + "ms");
+
+			LinkedList<int> linkedList = new LinkedList<int>();
+
+			temp = TestaAdcionaNoFinal.AdicionaNoFinal(linkedList);
+			Console.WriteLine("LinkedList: " + temp + "ms");
+		}
+
+		public static long AdicionaNoFinal(ICollection<int> lista)
+		{
+			Stopwatch sw = new Stopwatch();
+			sw.Start();
+			int size = 100000;
+
+			for (int i = 0; i < size; i++)
+			{
+				lista.Add(i);
+			}
+
+			sw.Stop();
+
+			return sw.ElapsedMilliseconds;
+		}
+	}
+}
